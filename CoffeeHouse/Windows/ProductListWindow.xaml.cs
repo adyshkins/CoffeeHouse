1b86f74413b21c2a22edc4a9f29f52bd2e826108
Сергей Адышкин <adyshkinss@outlook.com>
2023-02-20 16:15:31 +0300

    Список товаров

---
+++ CoffeeHouse/Windows/ProductListWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using CoffeeHouse.ClassHelper;
+using CoffeeHouse.DB;
+using static CoffeeHouse.ClassHelper.EFClass;
+
+namespace CoffeeHouse.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListWindow.xaml
+    /// </summary>
+    public partial class ProductListWindow : Window
+    {
+        public ProductListWindow()
+        {
+            InitializeComponent();
+
+            GetProducr();
+        }
+
+        private void GetProducr()
+        {
+            List<Stuff> stuffList = new List<Stuff>();
+
+            stuffList = Context.Stuff.ToList();
+
+            LvProductList.ItemsSource = stuffList;
+        }
+    }
+}
